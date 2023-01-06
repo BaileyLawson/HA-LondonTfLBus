83043dbf7da39c20368a4b1c117f3687e1a4fd5b
Bailey Lawson <41685901+BaileyLawson@users.noreply.github.com>
2023-01-06 16:44:05 +0000

    Update tfl_data.py

--- custom_components/london_tfl_bus/tfl_data.py
+++ custom_components/london_tfl_bus/tfl_data.py
@@ -77,9 +77,9 @@ class TfLData:
             departure = {
                 'time_to_station': time_to_station(item, False),
                 'platform': (
-                    item['platformName'] if 'platformName' in item else ''
+                    item['lineId'] if 'lineId' in item else ''
                 ),
-                'line': item['platformName'] if 'platformName' in item else '',
+                'line': item['lineId'] if 'lineId' in item else '',
                 'direction': 0,
                 'departure': item['expectedArrival'],
                 'destination': get_destination(item),
@@ -87,7 +87,7 @@ class TfLData:
                 'expected': item['expectedArrival'],
                 'type': 'Metros',
                 'groupofline': '',
-                'icon': 'mdi:train',
+                'icon': 'mdi:bus',
             }
 
             departures.append(departure)
